fb8acb47f69aeadae24fa4e25c46322d2e494d13
eastar93 <eastar93@naver.com>
2021-10-26 17:21:48 +0900

    eastar93

--- app/src/androidTest/java/com/widgetprj2/app/src/main/java/com/example/widgetprj2/MainActivity.java
+++ app/src/androidTest/java/com/widgetprj2/app/src/main/java/com/example/widgetprj2/MainActivity.java
@@ -31,28 +31,28 @@ public class MainActivity extends AppCompatActivity {
         TextView1 = (TextView)findViewById(R.id.TextView1);
 
 
-            Btn1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // 콘솔 디버깅1
-                    System.out.println("버튼 클릭됨");
-                    // 콘솔 디버깅 2 - cncjsc
-                    Log.i("태그명", "로깅할 내용");
-                    // 토스트 디버깅
-                    Toast.makeText(getApplicationContext(), "토스트로 디버깅 가능", Toast.LENGTH_SHORT).show();
-                    if (bool) {
-                        TextView1.setTextSize(50);
-                        TextView1.setTextColor(Color.BLUE);
-                        TextView1.setTypeface(Typeface.SERIF);
+        Btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 콘솔 디버깅1
+                System.out.println("버튼 클릭됨");
+                // 콘솔 디버깅 2 - cncjsc
+                Log.i("태그명", "로깅할 내용");
+                // 토스트 디버깅
+                Toast.makeText(getApplicationContext(), "토스트로 디버깅 가능", Toast.LENGTH_SHORT).show();
+                if (bool) {
+                    TextView1.setTextSize(50);
+                    TextView1.setTextColor(Color.BLUE);
+                    TextView1.setTypeface(Typeface.SERIF);
 
-                        TextView1.setText("On-ffice조 신동규");
-                        bool = false;
-                    } else {
-                        TextView1.setTextColor(Color.RED);
-                        bool = true;
-                    }
+                    TextView1.setText("On-ffice조 신동규");
+                    bool = false;
+                } else {
+                    TextView1.setTextColor(Color.RED);
+                    bool = true;
                 }
-            });
-        }
+            }
+        });
     }
+}
 
